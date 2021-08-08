637ddfe0cd4e05785dc78a1e4778278c9ab2e207
ash <ashwinnema06@gmail.com>
2021-08-08 06:58:24 +0530

    signup page added

--- apnamart/src/routes/index.js
+++ apnamart/src/routes/index.js
@@ -3,12 +3,14 @@ import Posts from '../components/Posts'
 import Post from '../components/Post'
 import Allposts from '../components/All posts'
 import { Login } from '../containers/login'
+import { Signup } from '../containers/signup'
 
 const routes = [
     {exact:true, path:PATHS.HOME, component:Posts},
     {exact:true,path:PATHS.POST, component:Post},
     {exact:true, path:PATHS.POSTS,component:Allposts},
-    {exact:true, path:PATHS.LOGIN, component:Login}
+    {exact:true, path:PATHS.LOGIN, component:Login},
+    {exact:true, path:PATHS.SIGNUP,component:Signup}
 ]
 
 export default routes
